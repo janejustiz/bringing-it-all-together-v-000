26b55ba83773aa78dc4bfb299a44ace9ace1b503
Jane Justiz <janejustiz@gmail.com>
2019-08-06 00:45:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,13 @@
 class Dog
   attr_accessor :id, :name, :breed
+  
   def initialize(id:nil, name:, breed:)
     @id=id
     @name=name
     @breed=breed
   end
+
+  def self.create_table
+  end
+  
 end
