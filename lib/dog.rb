76c891dcb3f0e51e1a5a31faa37ec8d7c1d965d6
Jane Justiz <janejustiz@gmail.com>
2019-08-06 01:42:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -61,7 +61,7 @@ class Dog
     if dog
       Dog.new_from_db(dog[0])
     else
-      self.create(id: dog[0], name: dog[1], breed: dog[2])
+      self.create(id: dog[0][0], name: dog[0][1], breed: dog[0][2])
     end
   end
 
